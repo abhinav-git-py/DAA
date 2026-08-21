57ca03e5c243822e2a32eac41f2031ffda9be900
abhinav-git-py <nimmapatiabhinav@gmail.com>
2026-08-21 15:56:50 +0530

    Update factorial.py

--- Practical-4/factorial.py
+++ Practical-4/factorial.py
@@ -1,8 +1,23 @@
-def factorial(n):
-    fact=1
-    for i in range(1,n+1):
-        fact=fact*i
+def factorial_i(n):
+    fact = 1
+
+    for i in range(1, n + 1):
+        fact = fact * i
+
     return fact
 
-n=int(input("Enter n: "))
-print("Factorial:",factorial(n))
+
+def factorial_r(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial_r(n - 1)
+
+
+n = int(input("Enter a number: "))
+
+print("Factorial using iteration =", factorial_i(n))
+
+n = int(input("Enter n: "))
+
+print("Factorial using recursion =", factorial_r(n))
